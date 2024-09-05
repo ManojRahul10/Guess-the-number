27373d703f9a253f7b0358cc69defb1f9cc840ba
ManojRahul10 <manojrahulkiddo35@gmail.com>
2024-09-05 23:11:17 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
+import Result from './result';
+
+const secretNumber = Math.floor(Math.random() * 10) + 1;
 
 function App() {
+
+  const [term,setTerm]=useState(''); 
+  const handleChange=(e)=> {
+    setTerm(e.target.value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className='head'>
+        <label htmlFor='term' > 
+          Guess the Number Between 1-10 (answer: 🖕)
+        </label>
+      </div>
+      <input id='term' type='text' name='number' onChange={handleChange} />
+      <Result className="result" secretNumber={secretNumber} term ={term}/>
     </div>
   );
 }
